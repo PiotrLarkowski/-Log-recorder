f1406b06b8425584dd5ca2f568edbfee474ba8a0
Piotr Larkowski <p.larkowski90@gmail.com>
2024-03-22 20:20:51 +0100

    Add first method to select message from warning

--- src/LogLevels.java
+++ src/LogLevels.java
@@ -1,3 +1,22 @@
 public class LogLevels {
-
+    public static String message(String logLine) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] separatedWords = logLine.split(" ");
+        for (int i = 1; i < separatedWords.length; i++) {
+            char[] charArray = separatedWords[i].toCharArray();
+            StringBuilder stringBuilder1 = new StringBuilder();
+            for (int j = 0; j < charArray.length; j++) {
+                if(charArray[i]=='\\'){
+                    break;
+                }else{
+                    stringBuilder1.append(charArray[j]);
+                }
+            }
+            stringBuilder.append(stringBuilder1);
+            if(i<separatedWords.length-1){
+                stringBuilder.append(" ");
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println(LogLevels.message("[ERROR]: Invalid operation"));
+        System.out.println(LogLevels.message("[WARNING]: Disk almost full"));
     }
 }
